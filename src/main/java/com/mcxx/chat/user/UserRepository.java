38a8ac0c9e30541825d7dbdeeb09bd75e3d9de84
QHung1209 <21522117@gm.uit.edu.vn>
2026-06-29 14:06:37 +0700

    feat(user): add user info caching
    Add UserCacheService and UserBasicInfo projection; adjust UserRepository.

--- src/main/java/com/mcxx/chat/user/UserRepository.java
+++ src/main/java/com/mcxx/chat/user/UserRepository.java
@@ -4,9 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByUsername(String username);
 
---
+++ src/main/java/com/mcxx/chat/user/dto/UserBasicInfo.java
@@ -0,0 +1,19 @@
+package com.mcxx.chat.user.dto;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class UserBasicInfo {
+  private UUID id;
+  private String username;
+  private String name;
+  private String email;
+  private String avatar;
+}
